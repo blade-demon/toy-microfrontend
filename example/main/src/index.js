b836cd0d7a1540b1810d3b0ff414b98d8da959f2
blade-demon <xuziwei89@gmail.com>
2022-02-20 21:29:43 +0800

    feat: step1-starter

--- example/main/src/index.js
+++ example/main/src/index.js
@@ -5,17 +5,17 @@ import App from './App'
 
 import { registerMicroApps, start } from 'qiankun';
 
-
+// 注册子应用
 registerMicroApps([
   {
-    name: 'vue1', // app name registered
-    entry: '//localhost:8080',
-    container: '#micro-container',
-    activeRule: '/vue1',
+    name: 'vue1', // 子应用名称
+    entry: 'http://localhost:8082', // 子应用入口链接
+    container: '#micro-container', // 挂载点
+    activeRule: '/vue1', // 子应用路由
   },
   {
     name: 'vue2',
-    entry: "http://localhost:8081/",
+    entry: "http://localhost:8083",
     container: '#micro-container',
     activeRule: '/vue2',
   },
--- example/sub/src/main.js
+++ example/sub/src/main.js
@@ -19,6 +19,8 @@ export async function bootstrap() {
  * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
  */
 export async function mount() {
+
+  // vue 应用渲染方法，不同的框架不同的方法
   render();
 }
 
@@ -26,6 +28,7 @@ export async function mount() {
  * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
  */
 export async function unmount() {
+  // 针对 vue的卸载方法，其他框架调用对应的卸载方法
   app.$destroy()
 }
 
